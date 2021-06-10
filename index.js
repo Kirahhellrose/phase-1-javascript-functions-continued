86586dc40d0edde7734669c793280ad9a08ce0be
Kirahhellrose <clr@DESKTOP-MVRDDCF.localdomain>
2021-06-10 09:24:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+//Saturday Fun
+const saturdayFun = function (activity="roller-skate"){
+    return console.log(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+//Monday Work 
+const mondayWork = function (grindStone = "go to the office"){
+    return console.log(`This Monday, I will ${grindStone}.`);
+}
+mondayWork();
+
+//Wrap Adjective 
+const wrapAdjective = function (cheer){
+    return console.log(`You are ${cheer}!!`);
+}
+wrapAdjective("special");
